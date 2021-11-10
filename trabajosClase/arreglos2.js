f2bb30356baee7a4244e91252b100f5a4e233198
Maikel Zambrano <zambranomaykel68@gmail.com>
2021-11-10 14:00:11 -0500

    talleres en clases

--- trabajosClase/arreglos2.js
+++
@@ -1,8 +0,0 @@
-const numeros =  new Set();
-
-numeros.add(2);
-numeros.add(3);
-numeros.add(2);
-
-
-console.log(numeros)
--- trabajosClase/arreglos3.js
+++
@@ -1,7 +0,0 @@
-const estudiantes =  new Map();
-
-estudiantes.set("11111","Carlos");
-estudiantes.set("22222","Alex");
-
-
-console.log( estudiantes.get("11111"))
