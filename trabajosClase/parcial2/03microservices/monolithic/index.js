0a50665c40d4981404c24f6ffa98d42f1ec66d11
Maikel Zambrano <zambranomaykel68@gmail.com>
2021-12-27 14:45:31 -0500

    creacion de la carpeta public

--- trabajosClase/parcial2/03microservices/monolithic/index.js
+++ trabajosClase/parcial2/03microservices/monolithic/index.js
@@ -1,4 +1,4 @@
-const server = require('./src/app');
+const server = require('./src/app.js');
 server.listen(process.env.PORT, ()=>{
     console.log(`monolithic server run... corriendo en puerto ${process.env.PORT}`)
 });
